0c4a4788e3815ed6f1da7b1ab03b4830cd1764a0
eugalata <eugalata4@gmail.com>
2014-04-24 02:11:20 +0300

    all

--- footer.inc.php
+++ footer.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "@Copyright 2013 MineNinja Released under <a href=\"http://www.gnu.org/copyleft/gpl.html\" target=\"_blank\">GPLv3</a>.";
+echo "@Copyright 2014 RackMime.com Released under <a href=\"http://www.gnu.org/copyleft/gpl.html\" target=\"_blank\">GPLv3</a>.";
 echo "<br />";
 
 ?>
