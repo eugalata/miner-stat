2c73c6a6a34058b700020a9ca22e0bc3403c7d42
eugalata <eugalata4@gmail.com>
2014-04-26 07:12:41 +0300

    Revert "all"
    This reverts commit 0c4a4788e3815ed6f1da7b1ab03b4830cd1764a0.
    
    Conflicts:
    	footer.inc.php

--- faq.php
+++ faq.php
@@ -47,12 +47,16 @@ ddsmoothmenu.init({
 				 
                 <div class="cleaner h20"></div>
 
-			
+			<h4>Whats this ?</h4>
+		MineNinja is a Beaglebone based device for managing USB-based bitcoin mining devices. MineNinja runs cgminer/or bfgminer as a service on the Beaglebone and uses Anubis as its web frontend.
+	Anubis "watches" your hosts by connecting to the API Port of cgminer (or bfgminer). You can easily add additional hosts to manage your whole operation using a single MineNinja.
   <div class="cleaner h20"></div>		<h4>How Do I add more hosts?</h4>
 			The Connection is very simple, just add "--api-listen" (and "--api-network") to the cgminer command line and cgminer's api is enabled. Then you can simply <a href="addhost.php">add a host here.</a></td>
   <div class="cleaner h20"></div>		<h4>Something is wrong/does not work as expected.</h4>
-			MineNinja and the open source software it is built on is still under heavy development.  Please visit rackmime.net for release updates.
- 
+			MineNinja and the open source software it is built on is still under heavy development.  Please visit mineninja.com for release updates.
+  <div class="cleaner h20"></div>		<h4>Installation?</h4>
+			MineNinja is pre-configured, just plug in your USB-based ASIC or FPGA devices to the USB ports on the MineNinja device. MineNinja runs the Angrstrom Linux distribution and is optimized for low power performance.
+			Some boards will require you to ssh to the MineNinja to modprobe the USB driver and restart the cgminer service.
      
         
 
--- footer.inc.php
+++ footer.inc.php
@@ -1,6 +1,10 @@
 <?php
 
+<<<<<<< HEAD
 echo "@Copyright 2014 RackMime.net Released under <a href=\"http://www.gnu.org/copyleft/gpl.html\" target=\"_blank\">GPLv3</a>.";
+=======
+echo "@Copyright 2013 MineNinja Released under <a href=\"http://www.gnu.org/copyleft/gpl.html\" target=\"_blank\">GPLv3</a>.";
+>>>>>>> parent of 0c4a478... all
 echo "<br />";
 
 ?>
